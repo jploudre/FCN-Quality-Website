f1e1db2a419a6137e4cffc3e0318ed17a4e841e2
Jonathan Ploudre <jploudre@gmail.com>
2019-04-08 09:39:56 -0700

    Updated Data

--- make_website.py
+++ make_website.py
@@ -14,9 +14,9 @@ import tqdm as tqdm
 from selenium import webdriver
 
 create_graphs = True
-create_svgs = False
+create_svgs = True
 create_htmls = True
-create_pdfs = False
+create_pdfs = True
 
 names = pd.read_csv("./files/names.csv", index_col="MeridiosName")
 metrics = pd.read_csv(
